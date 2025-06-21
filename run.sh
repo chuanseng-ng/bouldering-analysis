7898319e03c139fdb6ef233e72dc2947f250a2f2
Ng Chuan Seng <waelectriz@gmail.com>
2025-06-21 13:25:04 +0800

    [Code] Move code into src directory + Setup README.md inside src + Add run.sh at top

---
+++ run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Will move to src directory to execute main.py"
+cd src
+echo "Move done!"
+echo ""
+echo "Will now execute main.py"
+python main.py
+echo "Script execution is completed!"
