771f085b8362a0e74c68f0da1a4ed302f934a11e
Ng Chuan Seng <waelectriz@gmail.com>
2025-11-30 21:55:40 +0800

    [Fix] Comments on script type

--- run_setup_dev.sh
+++ run_setup_dev.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -euo pipefail
-# run_setup_dev.csh: C-shell script to run the development setup script
+# run_setup_dev.csh: Bash-shell script to run the development setup script
 
 python -m pip install -r requirements.txt
 python setup_dev.py
